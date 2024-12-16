86c5b3c1cd21b07f19a40ce15ce43f6eead1a4d8
Ashish Kumar <ashishkumar@Ashishs-MacBook-Air.local>
2024-12-16 19:07:25 +0530

    added framer-motion

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,13 +3,21 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 3 years of hands-on experience, I have honed my skills in back-end technologies like SpringBoot, REST APIs, Java, PostgreSQL and Hbase, as well as front-end technologies like React . My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate software developer with a knack for crafting robust and scalable web applications. With 3 years of hands-on experience, I have honed my skills in back-end technologies like SpringBoot, REST APIs, Java, PostgreSQL and Hbase, as well as front-end technologies like React . My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile software developer with a passion for creating efficient web applications. With 3 years of professional experience, I have worked with a variety of technologies, including SpringBoot, REST APIs, Java, PostgreSQL, React, PostgreSQL, Docker , Kubernetes , Jenkins, Git , Linux etc. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2022- Present",
+    year: "2023-2024",
+    role: "Software Developer",
+    company: "Amdocs",
+    description: `Part of AT&T Cricket where implemented robust and scalable microservices,leveraging  technologies
+    like Java 17 , CassandraDB , Azure Devops and Oracle SQL to manage key business processes efficiently.Utilized docker and kubernetes for containerization and orchestration,enabling smooth deployment and scaling of microservices on Azure DevOps.`,
+    technologies: ["Springboot", "Java", "Microservices", "Azure","Docker","Kubernetes"],
+  },
+  {
+    year: "2022-2023",
     role: "Software Developer",
     company: "Amdocs",
     description: `Part of AT&T Mexico team, developed and suppotred applications using Springboot, REST APIs, and Java. Implemented RESTful APIs and integrated with Postgress,Cassandra databases. Collaborated with stakeholders to define project requirements and timelines.`,
@@ -56,7 +64,7 @@ export const EXPERIENCES = [
 ];
 */
 export const CONTACT = {
-  address: "Magarpatta Pune Maharashtra 411028",
+  address: "Bengaluru Karnatka",
   phoneNo: "+91-9304163063",
   email: "krashishparker@gmail.com",
 };
